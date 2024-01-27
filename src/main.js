d962f1ed747793b600eb79d8c20009c236c3d89a
izzwerg <izzwerg.kr@gmail.com>
2024-01-28 00:26:37 +0200

    fin

--- src/main.js
+++ src/main.js
@@ -8,8 +8,10 @@ const form = document.querySelector('.form');
 form.addEventListener('submit', imageSearch);
 const gallery = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
+const moreButton = document.querySelector('.is-hidden');
 const API_KEY = '11329962-6436ba51ddb58bb96deed169a';
 let pageNumber;
+let searchTerm;
 
 axios.defaults.baseURL = 'https://pixabay.com/api';
 
@@ -20,27 +22,58 @@ async function imageSearch(e) {
   loader.classList.add('is-visible');
 
   const form = e.currentTarget;
-  const searchTerm = form.elements.searchTerm.value;
+  searchTerm = form.elements.searchTerm.value;
 
   try {
-    const images = await getImages(searchTerm);
+    let images = await getImages(searchTerm);
 
-    if (images.totalHits === 0) {
+    if (images.total === 0) {
       noFoundMessage();
     } else {
-      let markup = '';
-      for (const image of images.hits) {
-        markup += createMarkup(image);
-      }
-      loader.classList.remove('is-visible');
-      gallery.insertAdjacentHTML('beforeend', markup);
-      const lightbox = new SimpleLightbox('.gallery a');
-      lightbox.refresh();
+      addMarkup(images);
     }
-  } catch (error){
-    console.log(error);
+  } catch (error) {
+    errorMessage(error.message);
   } finally {
     form.reset();
+  }
+}
+
+async function addMoreImages() {
+  loader.classList.add('is-visible');
+  try {
+    let images = await getImages(searchTerm);
+    addMarkup(images);
+    let elem = gallery.querySelector('li');
+    let rect = elem.getBoundingClientRect();
+    scrollBy({
+      top: rect.height * 2 + 48,
+      behavior: 'smooth',
+    });
+  } catch (error) {
+    errorMessage(error.message);
+  }
+}
+
+function addMarkup(images) {
+  moreButton.classList.remove('is-visible');
+  let markup = '';
+  for (const image of images.hits) {
+    markup += createMarkup(image);
+  }
+  loader.classList.remove('is-visible');
+  gallery.insertAdjacentHTML('beforeend', markup);
+  const lightbox = new SimpleLightbox('.gallery a');
+  lightbox.refresh();
+  checkMore(images.totalHits);
+}
+
+function checkMore(maxImages) {
+  if (gallery.querySelectorAll('li').length < maxImages) {
+    moreButton.addEventListener('click', addMoreImages);
+    moreButton.classList.add('is-visible');
+  } else {
+    noMoreMessage();
   }
 }
 
@@ -56,6 +89,7 @@ async function getImages(searchTerm) {
   });
 
   const response = await axios.get(`/?${urlParams}`);
+  pageNumber++;
   return response.data;
 }
 
@@ -92,4 +126,27 @@ function noFoundMessage() {
       'Sorry, there are no images matching your search query. Please try again!',
   });
   loader.classList.remove('is-visible');
+}
+
+function errorMessage(msg) {
+  iziToast.show({
+    position: 'topRight',
+    messageColor: 'white',
+    iconUrl: 'attention.png',
+    iconColor: 'white',
+    color: '#EF4040',
+    message: msg,
+  });
+  loader.classList.remove('is-visible');
+}
+
+function noMoreMessage() {
+  iziToast.show({
+    position: 'topRight',
+    messageColor: 'white',
+    iconUrl: 'fin.png',
+    iconColor: 'white',
+    color: '#4E75FF',
+    message: "We're sorry, but you've reached the end of search results.",
+  });
 }
